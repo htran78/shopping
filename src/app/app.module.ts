319d9d746140a4caf53f31784c3271469f0b0ac4
htran78 <peteyhanh@gmail.com>
2017-10-21 19:20:34 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './../app/components/root/app.component';
+import { MainHeader } from './../app/components/common/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeader
   ],
   imports: [
     BrowserModule
